4a9061efeb293d0835f7d364a5aad55b7bc1ab32
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-10-10 16:50:46 +0200

    wip form update and add dependencies for later

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,70 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main where
 
 import Web.Hyperbole
 import Web.Atomic.CSS
 import Data.Text
+import Effectful
+
+
+data AppColor
+  = White
+  | LightGray
+  | DarkGray
+  | DarkRed
+  deriving (Show)
+
+
+instance ToColor AppColor where
+  colorValue White = "#FFF"
+  colorValue LightGray = "#D3D3D3"
+  colorValue DarkGray = "#626262"
+  colorValue DarkRed = "#800000"
+
+
+
+newtype Submission f = Submission
+  { program :: Field f Text
+  }
+  deriving (Generic, FromFormF, GenFields FieldName, GenFields Validated)
+
+
+data Feedback = Feedback
+  deriving (Generic, ViewId)
+
+
+instance IOE :> es => HyperView Feedback es where
+  data Action Feedback
+    = Submit
+    deriving (Generic, ViewAction)
+  update Submit = do
+   -- f <- formData @(Submission Identity)
+    liftIO $ print @Int 2
+    pure $ do
+      tAreaForm
+      -- el "submitted"
+
+
+
+tAreaForm :: View Feedback ()
+tAreaForm = form Submit ~ grow $ do
+  -- let f = fieldNames @Submission
+  field "program" $ do
+    header "Code Input"
+    textarea Nothing ~ bg LightGray . grow  -- . height (PxRem 500)
+  submit "Submit" ~ bg DarkRed . color White
 
 
 
@@ -20,37 +74,40 @@ main = do
     liveApp quickStartDocument (runPage page)
 
 
-page :: Page es '[]
-page = pure $ do
-  row $ do
-    header "Title"
-    space
-    el ~ stack $ do
-      tag "button" "Load Example" @ class_ "dropdown-examples"
-      ol ~ popup (TL 30 10) . display None . displayOnHover $ do
-        let numerals = list Decimal
-        li ~ numerals $ "one"
-        li ~ numerals $ "two"
-        li ~ numerals $ "three"
-    space
-    nav $ do
-      link [uri|https://fmidue.github.io/codeworld-tasks/|] "Docs"
-      link [uri|https://github.com/fmidue/codeworld-tasks|] "Repo"
-  row ~ pad 10 . gap 10 $ do
-    col $ do
-      header "Code Input"
-      img "https://placehold.co/1200x800"
-    col $ do
-      header "Feedback"
-      img "https://placehold.co/1200x800"
-  header "run"
+page :: IOE :> es => Page es '[Feedback]
+page = do
+  liftIO $ print @Int 1
+  pure $ do
+    row ~ bg DarkRed . color White $ do
+      header "Title"
+      space
+      el ~ stack @ class_ "dropdown-examples" $ do
+        buttonMock "Load Example"
+        ol ~ popup (TL 20 10) . visibility Hidden . displayOnHover $ do
+          let numerals = list Decimal
+          li ~ numerals $ "Task01"
+          li ~ numerals $ "Task03"
+          li ~ numerals $ "Task08"
+      space
+      nav $ do
+        link [uri|https://fmidue.github.io/codeworld-tasks/|] "Docs"
+        link [uri|https://github.com/fmidue/codeworld-tasks|] "Repo"
+    row ~ grow . pad 10 . gap 10 ~ bg DarkGray $ do
+      hyper Feedback tAreaForm ~ display Flex . grow
+      col ~ grow $ do
+        header "Feedback"
+        el "" ~ bg LightGray . grow
 
+
+
+buttonMock :: View c () -> View c ()
+buttonMock = tag "button"
 
 displayOnHover :: Styleable h => CSS h -> CSS h
 displayOnHover = css
     ""
-    ".dropdown-examples:hover + ol"
-    [ "display" :. "block"
+    ".dropdown-examples:hover ol"
+    [ "visibility" :. "visible"
     ]
 
 
